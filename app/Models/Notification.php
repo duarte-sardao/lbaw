b540c1c05b8ed24fbc2ab142a4a884e7d6e98edf
Nuno Miguel Carvalho de Jesus <up201905477@fe.up.pt>
2021-12-10 10:24:46 +0000

    Upload New File

---
+++ app/Models/Notification.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model {
+    
+    //The relation does not have created_at and updated_at columns
+    public $timestamps = false;
+
+    //Defines the table's name
+    protected $table = 'Notification';
+    
+    //Attributes of the relation that can be modified upon creation or update
+    protected $fillable = ['id', 'content', 'id_Customer'];
+}
+
+?>
