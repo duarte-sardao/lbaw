17842b02613dfa9fa8b34f02230b2e6fbeffbe04
Nuno Jesus <nuno67713@DESKTOP-1434JL2.localdomain>
2021-12-23 15:50:46 +0000

    Adding blade templates for some pages.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/Home';
+    protected $redirectTo = '/users/{id}';
 
     /**
      * Create a new controller instance.
