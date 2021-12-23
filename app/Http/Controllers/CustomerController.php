e0e5a8c2e64e41073d1ddbf45d022796c88da131
Nuno Jesus <nuno67713@DESKTOP-1434JL2.localdomain>
2021-12-23 13:38:11 +0000

    Implemented the body of update and delete methods in CustomerController.php and commented every method.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,25 +7,33 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 { 
-   //Displays creation form
+  /**
+   * @method Displays the register form
+   */
   public function create()
   {
     return view('pages.register');
   }
 
-  //Stores a new Customer created in create()
+  /**
+   * @method Processes the data inputed on the creation form to create a new instance of the Customer Model
+   * @param request HTTP request with the input data
+   * @return customer The new customer
+   */
   public function store(Request $request)
   {
-    $Customer = new Customer();
-    $Customer->id = $request->input('id');
-    $Customer->cart_id = $request->input('id');
-    $Customer->save();
+    $customer = new Customer();
+    $customer->id = $request->input('id');
+    $customer->cart_id = $request->input('id');
+    $customer->save();
 
-    return $Customer;
-
+    return $customer;
   }
   
-  //Displays the Customer's profile
+  /**
+   * @method Displays the customer's profile
+   * @param id Id of the customer whose profile will be displayed
+   */
   public function show($id)
   { 
     $customer->Customer::find($id);
@@ -34,7 +42,10 @@ class CustomerController extends Controller
     return view('pages.user_profile', ['customer' => $customer]);
   }
 
-  //Displays edit form
+  /**
+   * @method Displays the edit profile form
+   * @param id Id of the customer whose profile will be edited
+   */
   public function edit($id)
   {
     $customer->Customer::find($id);
@@ -43,16 +54,26 @@ class CustomerController extends Controller
     return view('pages.edit_profile', ['customer'=> $customer]);
   }
 
-  //Commits the changes made in edit
+  /**
+   * @method Processes the data inputed on the edit profile form to update the customer's data
+   * @param id Id of the user to delete
+   */
   public function update(Request $request, $id)
   {
-    
+    $account = Account::find($id);
+    return $account->update($request, $id);
   }
 
-  //Deletes the Customer from the database
-  public function destroy($id)
+  /**
+   * @method Deletes a user from the database, by deleting its account
+   * @param id Id of the user to delete
+   * @return ---
+   */
+  public function delete($id)
   {
-    
+    $account = Account::find($id);
+    $this->authorize('delete', $account);
+    return $account->destroy($request, $id);
   }
 }
 
