af42000d925e13816befffd9100b6a0a3195e0e2
Nuno Jesus <nunomiguel533@gmail.com>
2022-01-05 01:34:47 +0000

    Overhauled the singe product page layout and added logic to retrieve specific product details (not sure how this will work yet).

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\CPU;
+use App\Models\GPU;
+use App\Models\Motherboard;
+use App\Models\Cooler;
+use App\Models\PowerSupply;
+use App\Models\Storage;
+use App\Models\PcCase;
+use App\Models\Other;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,6 +31,68 @@ class ProductController extends Controller
 
   public function getAllProducts(){
     return view('pages.products.products_list', ['results' => Product::all()]);
+  }
+  
+  public function showProduct($id){
+    $product = Product::find($id);
+    $details = $this->retrieveProductDetails($id);
+    
+    return view('pages.products.product', [
+      'product' => $product,
+      'details' => $details
+    ]);
+  }
+  
+  public function retrieveProductDetails($id){
+    if(CPU::find($id)->exists()) return $this->retrieveCpuDetails($id);
+    else if(GPU::find($id)->exists()) return $this->retrieveGpuDetails($id);
+    else if(Motherboard::find($id)->exists()) return $this->retrieveMotherboardDetails($id);
+    else if(Cooler::find($id)->exists()) return $this->retrieveCoolerDetails($id);
+    else if(PowerSupply::find($id)->exists()) return $this->retrievePowerSupplyDetails($id);
+    else if(Storage::find($id)->exists()) return $this->retrieveStorageDetails($id);
+    else if(PcCase::find($id)->exists()) return $this->retrievePcCaseDetails($id);
+    else return $this->retrieveOtherDetails($id);
+  }
+    
+
+  public function retrieveCpuDetails($id){
+    $product = Cpu::find($id);
+    return null;
+  }
+  
+  public function retrieveGpuDetails($id){
+    $product = Gpu::find($id);
+    return null;
+  }
+  
+  public function retrieveMotherboardDetails($id){
+    $product = Motherboard::find($id);
+    return null;
+  }
+  
+  public function retrieveCoolerDetails($id){
+    $product = Cooler::find($id);
+    return null;
+  }
+  
+  public function retrievePowerSupplyDetails($id){
+    $product = PowerSupply::find($id);
+    return null;
+  }
+  
+  public function retrieveStorageDetails($id){
+    $product = Storage::find($id);
+    return null;
+  }
+  
+  public function retrievePcCaseDetails($id){
+    $product = PcCase::find($id);
+    return null;
+  }
+  
+  public function retrieveOtherDetails($id){
+    $product = Other::find($id);
+    return null;
   }
 }
 ?>
--- resources/views/pages/products/product.blade.php
+++ resources/views/pages/products/product.blade.php
@@ -6,45 +6,87 @@
   <nav class = "m-3" aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+      <li class="breadcrumb-item"><a href="{{route('allProducts')}}">Products</a></li>
       <li class="breadcrumb-item" aria-current="page">{{$product->name}}</li>
     </ol>
   </nav>
 
-  <div class = "container mt-5" id="homeProductGrid">
-    <div class="row d-flex m-1">
-      <div class="card col m-1">
+  <div class = "container-fluid mt-2">
+    <div class="row m-5 mt-1">
+      <div class="card col-md m-1">
         <div class="card-body d-flex flex-column justify-content-between">
-          <a href = "#">
-            <img src = {{asset($product->image)}} width = "100%">
-          </a>
-          <a> 
-            *{{$product->rating}} 
-          </a>
+          <img src = {{asset($product->image)}} width = "100%">
         </div>
+      </div>
 
-        <div class="product-info d-flex flex-column">
-          <strong>{{$product->name}}</strong>
-          <small>{{$product->description}}</small>
-          
-          <div class="d-flex flex-row">
-            <h3 class="mt-2 price">Price: {{$product->price}}€</h3>
-            <a> Stock: {{$product->stock}}</a>
-            <button type="button">Add To Wishlist</button>
-            <button type="button">Add to Cart</button>
+      <div class="col-md product-info d-flex flex-column">
+        <h4>{{$product->name}}</h4>
+        <span class = "mt-2 mb-2">{{$product->description}}</span>
+        
+        <div class="d-flex justify-content-between align-items-center">
+          <h3 class="mt-2 price">{{$product->price}}€</h3>
+
+          @if($product->stock > 0)
+            <span class = "text-success">
+              <i class = "fa fa-check"></i> In Stock
+            </span>
+          @else
+            <span class = "text-danger">
+              <i class = "fa fa-times"></i> Out of Stock
+            </span>
+          @endif
+
+          <div class="w-50 d-flex justify-content-between">
+            <button class = "btn btn-outline-danger w-100 m-1" type="button">
+              <i class = "fa fa-heart"></i>
+              <span>Add to Wishlist</span> 
+            </button>
+            <button class = "btn btn-outline-primary w-100 m-1" type="button">
+              <i class = "fa fa-cart-plus"></i>
+              <span>Add to Cart</span> 
+            </button>
           </div>
         </div>
       </div>
     </div>
-  
-    <div class="card col m-1">
-      <div class="card-body d-flex flex-column justify-content-between">
-        <button type="button">Details</button>
-        <button type="button">Reviews</button>
-      </div>
 
-      <div class="product-info d-flex flex-column">
-        <p>blah blah details reviews</p>
-      </div>
+    <div class="row m-5">
+      <h4>Details</h4>
+      <hr>
+    </div>
+
+    <div class="row m-5">
+      <h4>Reviews</h4>
+      <hr>
+
+      <form class = "card border-primary mt-4">
+        <h4 class="card-title  m-2">
+          Write your review!
+        </h4>
+        <div class="form-group m-2">
+          <label class = "" for="rating"><strong>Rating</strong></label>
+          <select class="form-control" id="rating">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+
+        <div class="form-group m-2">
+          <label class = "" for="coment"><strong>Your review</strong></label>
+          <textarea class="form-control" id="coment" rows="3"></textarea>
+        </div>
+
+        <div class="form-group m-2">
+          <button class="btn btn-outline-success" type = "submit">Submit</button>
+        </div>
+      </form>
+
+      @foreach($product->reviews as $review)
+        @include('partials.product.review')
+      @endforeach
     </div>
   </div> 
 @endsection
