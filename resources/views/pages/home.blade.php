dcd1fc9952b645cdee26c2b36327097074761220
Nuno Jesus <nunomiguel533@gmail.com>
2022-01-05 22:58:56 +0000

    Shifted the slider html code into a partials file. Cards size bug was fixed.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,38 +2,18 @@
 
 @section('content')
   <section class = "d-flex flex-column" id="content">
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="{{asset('images/img1.jpg')}}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{asset('images/img1.jpg')}}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{asset('images/img1.jpg')}}" class="d-block w-100" alt="...">
-        </div>
-      </div>
+    @include('partials.home.slider')
 
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-
-    <div class = "container mt-5" id="homeProductGrid">
+    <div class = "card-deck container mt-5" id="homeProductGrid">
       <div class="row">
         <h5>Most ordered products:</h5>
       </div>
       
-      <div class="row card-deck d-flex m-1">
+      <div class="row d-flex m-1">
         @foreach($productsList1 as $product)
-          @include('partials.product.card', ['product' => $product])
+          <div class="col-md">
+            @include('partials.product.card', ['product' => $product])
+          </div>
         @endforeach
       </div>  
 
@@ -41,9 +21,11 @@
         <h5>Products you might like:</h5>
       </div>
   
-      <div class="row card-deck d-flex m-1">
+      <div class="row d-flex m-1">
         @foreach($productsList2 as $product)
-          @include('partials.product.card', ['product' => $product])
+          <div class="col-md">
+            @include('partials.product.card', ['product' => $product])
+          </div>
         @endforeach
       </div>  
   </section>
