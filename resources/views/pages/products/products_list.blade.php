f3564e14154e646aeb6d212fb2c310c3f0bf8635
Nuno Jesus <nunomiguel533@gmail.com>
2022-01-09 03:06:15 +0000

    Corrected the display of a product list when display a number of products not divisible by 4 (number of columns).

--- resources/views/pages/products/products_list.blade.php
+++ resources/views/pages/products/products_list.blade.php
@@ -7,7 +7,7 @@
       <p>Your search retrieved <strong>{{count($products)}} products</strong>.</p>
       
       @php($i = 0)
-      <div class="row m-3">
+      <div class="row row-columns-4 m-3">
         @foreach($products as $product)
           <div class="col-md">
             @include('partials.product.card', ['product' => $product])
@@ -17,9 +17,13 @@
           <!-- Makes rows of 4 products each -->
           @if($i % 4 == 0)
             </div>
-            <div class="row m-3">
+            <div class="row row-columns-4 m-3">
           @endif
         @endforeach  
+
+        @for($i = 0; $i < 4 - count($products) % 4; $i++)
+          <div class="col-md"></div>
+        @endfor
       </div>
     
     @else
