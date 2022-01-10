68ad51f7efa505dfb1b9f3b8a8b481c03a491a0b
Carlos <up201907716@up.pt>
2022-01-10 01:31:19 +0000

    About us section looking better

--- resources/views/pages/static/about.blade.php
+++ resources/views/pages/static/about.blade.php
@@ -3,61 +3,66 @@
 @section('content')
     <div class="aboutus-section">
         <h1>About Us Page</h1>
-        <p>Some text about who we are and what we do.</p>
-        <p>Resize the browser window to see that this page is responsive by the way.</p>
     </div>
 
-    <h2 style="text-align:center">Our Team</h2>
     
     <div class="aboutus-row">
         <div class="aboutus-column">
             <div class="aboutus-card">
-                <img src="default.jpg" alt="Carlos" style="width:100%">
+                <img id= "developperPhoto" src="images/default.jpg" alt="Carlos">
                 <div class="aboutus-container">
                     <h2>Carlos Veríssimo</h2>
                     <p class="aboutus-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>Some text that describes me.</p>
                     <p>up201907716@up.pt</p>
-                    <p><button class="aboutus-button">Contact</button></p>
+                    <button class = "btn btn-outline-primary w-50 m-2">
+                        <a class="w-50 m-2" href="mailto:up201907716@up.pt"> Contact me! </a>
+                    </button>
                 </div>
             </div>
         </div>
 
         <div class="aboutus-column">
             <div class="aboutus-card">
-                <img src="/w3images/team2.jpg" alt="Duarte" style="width:100%">
+                <img id= "developperPhoto" src="images/default.jpg" alt="Duarte">
                 <div class="aboutus-container">
                     <h2>Duarte Sardão</h2>
                     <p class="aboutus-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>Some text that describes me.</p>
                     <p> up201905497@up.pt</p>
-                    <p><button class="aboutus-button">Contact</button></p>
+                    <button class = "btn btn-outline-primary w-50 m-2">
+                        <a class="w-50 m-2" href="mailto:up201905497@up.pt"> Contact me! </a>
+                    </button>
                 </div>
             </div>
         </div>
         
         <div class="aboutus-column">
             <div class="aboutus-card">
-                <img src="/w3images/team3.jpg" alt="Nuno" style="width:100%">
+                <img id= "developperPhoto" src="images/default.jpg" alt="Nuno">
                 <div class="aboutus-container">
                     <h2>Nuno Jesus</h2>
                     <p class="aboutus-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p> up201905477@up.pt</p>
-                    <p><button class="aboutus-button">Contact</button></p>
-                    </div>
+                    <p>Some text that describes me.</p>
+                    <p>up201905477@up.pt</p>
+                    <button class = "btn btn-outline-primary w-50 m-2">
+                        <a class="w-50 m-2" href="mailto:up201905477@up.pt"> Contact me! </a>
+                    </button>
                 </div>
             </div>
+        </div>
 
         <div class="aboutus-column">
             <div class="aboutus-card">
-                <img src="/w3images/team3.jpg" alt="Tomás" style="width:100%">
+                <img id= "developperPhoto" src="images/default.jpg" alt="Tomás">
                 <div class="aboutus-container">
                     <h2>Tomás Torres</h2>
                     <p class="aboutus-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p> up201800700@up.pt</p>
-                    <p><button class="aboutus-button">Contact</button></p>
+                    <p>Some text that describes me.</p>
+                    <p>up201800700@up.pt</p>
+                    <button class = "btn btn-outline-primary w-50 m-2">
+                        <a class="w-50 m-2" href="mailto:up201800700@up.pt"> Contact me! </a>
+                    </button>
                 </div>
             </div>
         </div>
