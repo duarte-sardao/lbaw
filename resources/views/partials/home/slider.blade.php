652848bc77af53ba18efd3933d3828b18a822d48
Nuno Jesus <nunomiguel533@gmail.com>
2022-01-10 13:26:49 +0000

    Created new images for the home slider and refactored the slider code.

--- resources/views/partials/home/slider.blade.php
+++ resources/views/partials/home/slider.blade.php
@@ -1,27 +1,39 @@
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src = {{asset('images/img1.jpg')}} class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src = {{asset('images/img1.jpg')}} class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src = {{asset('images/img1.jpg')}} class="d-block w-100" alt="...">
-    </div>
+    @include('partials.home.slider_active_entry', [
+      'name' => 'Gigabyte Radeon RX 6900 XT',
+      'text' => 'Lorem ipsum dolor sit amet',
+      'img' => 'images/img1.jpg',
+      'link' => 'products/18'
+      ])
+
+    @include('partials.home.slider_entry', [
+      'name' => 'ASRock TRX40 Taichi',
+      'text' => 'Lorem ipsum dolor sit amet',
+      'img' => 'images/img2.jpg',
+      'link' => 'products/28'
+      ])
+
+    @include('partials.home.slider_entry', [
+      'name' => 'AMD Ryzen Threadripper 3990X',
+      'text' => 'Lorem ipsum dolor sit amet',
+      'img' => 'images/img3.jpg',
+      'link' => 'products/6'
+      ])
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
---
+++ resources/views/partials/home/slider_active_entry.blade.php
@@ -0,0 +1,9 @@
+<div class="carousel-item active">
+  <a href="{{$link}}">
+    <img src="{{asset($img)}}" class="d-block w-100" alt="...">
+  </a>
+  <div class="carousel-caption d-none d-md-block">
+    <h5>{{$name}}</h5>
+    <p>{{$text}}</p>
+  </div>
+</div>
