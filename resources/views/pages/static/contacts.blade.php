81edb9f11624da0a120e9ff41725b9b1bd2777dd
Carlos <up201907716@up.pt>
2022-01-10 02:28:21 +0000

    Changes to the contacts page

--- resources/views/pages/static/contacts.blade.php
+++ resources/views/pages/static/contacts.blade.php
@@ -1,58 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+    
     <div class="contacts-row">
         <div class="contacts-column">
-            <div class="contacts-card">
-                <img src="default.jpg" alt="Carlos" style="width:100%">
-                <div class="contacts-container">
-                    <h2>Carlos Veríssimo</h2>
-                    <p class="contacts-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p>up201907716@up.pt</p>
-                    <p><button class="contacts-button">Contact</button></p>
-                </div>
-            </div>
-        </div>
+            <div class="contacts-card">    
+                <h2 class="contacts-heading">Contact Contact form</h2>
+                <div class="form">
+                        <form action="#">
+                        <label for="fname">First Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name..">
 
-        <div class="contacts-column">
-            <div class="contacts-card">
-                <img src="/w3images/team2.jpg" alt="Duarte" style="width:100%">
-                <div class="contacts-container">
-                    <h2>Duarte Sardão</h2>
-                    <p class="contacts-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p> up201905497@up.pt</p>
-                    <p><button class="contacts-button">Contact</button></p>
+                        <label for="lname">Last Name</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                        <label for="lname">Email Address</label>
+                        <input type="text" id="email" name="email" placeholder="Your Email Address..">
+
+                        <label for="subject">Subject</label>
+                        <textarea id="subject" name="subject" placeholder="Write something.." style="height:100px"></textarea>
+                        <input type="submit" value="Submit">
+                        </form>
+                    
                 </div>
             </div>
         </div>
         
         <div class="contacts-column">
-            <div class="contacts-card">
-                <img src="/w3images/team3.jpg" alt="Nuno" style="width:100%">
-                <div class="contacts-container">
-                    <h2>Nuno Jesus</h2>
-                    <p class="contacts-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p> up201905477@up.pt</p>
-                    <p><button class="contacts-button">Contact</button></p>
-                    </div>
-                </div>
-            </div>
-
-        <div class="contacts-column">
-            <div class="contacts-card">
-                <img src="/w3images/team3.jpg" alt="Tomás" style="width:100%">
-                <div class="contacts-container">
-                    <h2>Tomás Torres</h2>
-                    <p class="contacts-title">Developper</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p> up201800700@up.pt</p>
-                    <p><button class="contacts-button">Contact</button></p>
+            <div class="contacts-card"> 
+                <h2 class="contacts-heading"> Where were located at</h2>
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1894.472395791228!2d-8.596225921265447!3d41.17810868120167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd246446d48922a3%3A0x8b1e4a0bcdacc840!2sFEUP%20-%20Faculdade%20de%20Engenharia%20da%20Universidade%20do%20Porto!5e1!3m2!1spt-PT!2spt!4v1641778891426!5m2!1spt-PT!2spt" width="600" height="450"  allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
         </div>
-    </div>
+    </div>                   
 
-@endsection
+    @endsection
