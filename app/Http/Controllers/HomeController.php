c86b3084d453dfe41ea41a613ad7ede4193f9200
Carlos <up201907716@up.pt>
2022-01-14 20:22:01 +0000

    First row of products are now the products that are in most users carts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\CartProduct;
 
 class HomeController extends Controller
 {
     public function home(){
+        
+        $products = Product::all();
         $productsList1 = array();
         $productsList2 = array();
-        $products = Product::all();
 
+        $inMostCarts = CartProduct::groupBy('id_product')
+            ->selectRaw('count(*) as cnt, id_product')
+            ->orderBy('cnt', 'desc')
+            ->take(4)
+            ->get();
+        
+        foreach($inMostCarts as $entry){
+            array_push($productsList1, Product::find($entry->id_)); 
+        }
+        
         for($i = 0; $i < 4; $i++){
-            array_push($productsList1, $products[rand(0, count($products) - 1)]);
             array_push($productsList2, $products[rand(0, count($products) - 1)]);
         }
 
