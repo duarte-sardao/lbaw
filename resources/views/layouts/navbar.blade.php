8ceeb45b7ed8cc9ae0e9a0a3ce8207a9d70440a7
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-01-19 02:05:07 +0000

    Changed the navbar into an almost responsive one.

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,63 +1,71 @@
-<header class="navbar navbar-expand-lg bg-dark sticky-top justify-content-between">
-  <!-- Navbar left container -->
-  <div class="navbar-nav d-flex align-items-center">
-    <div class="logo">
-      <a class="navbar-brand m-2" href = "{{ route('home') }}">Grab N' Build</a>
-    </div>
 
-    <!-- Categories Dropdown -->
-    <div class="dropdown m-2">
-      <button class="btn btn-secondary dropdown-toggle" id = "categoriesDropdown" data-toggle="dropdown">
-        Categories
-      </button>
-      <nav class="dropdown-menu">
-        <a class="dropdown-item" href="{{url('products/categories/CPU')}}">CPUs</a>
-        <a class="dropdown-item" href="{{url('products/categories/GPU')}}">GPUs</a>
-        <a class="dropdown-item" href="{{url('products/categories/Motherboard')}}">Motherboards</a>
-        <a class="dropdown-item" href="{{url('products/categories/Storage')}}">Storage</a>
-        <a class="dropdown-item" href="{{url('products/categories/PcCase')}}">Cases</a>
-        <a class="dropdown-item" href="{{url('products/categories/Cooler')}}">Coolers</a>
-        <a class="dropdown-item" href="{{url('products/categories/PowerSupply')}}">Power Supplies</a>
-        <a class="dropdown-item" href="{{url('products/categories/Other')}}">Other</a>
-      </nav>
-    </div>
-  </div>  
-
-  <!-- Search bar -->
-  <form class="form-inline d-flex w-50" method = "POST" action = "{{route('search')}}">
-    @csrf
-    
-    <input class="form-control mr-sm-2" type="search" name = "search" placeholder="Search">
-  </form>
-  
-  <!-- User features -->
-  <div class = "navbar-nav d-flex">
-    @if (Auth::check())
-      <a title="Profile" class="btn btn-primary m-2" href = "{{route('profile')}}">
-        <i class="fa fa-user"></i>
-        <span>{{Auth::user()->username}}</span>
-      </a> 
-
-      <a title="Notifications" class="btn btn-primary m-2" href = "{{route('home')}}">    {{-- Change this --}}
-          <i class="fa fa-bell"></i>
-        </a> 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+  <div class="container-fluid d-flex jusityf-content-between">
+    <a class="navbar-brand" href="{{ route('home') }}">Grab N' Build</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+      <!-- Categories Dropdown -->
+      <ul class="navbar-nav me-auto mb-lg-0 align-items-center w-75">
+        <li>
+          <div class="dropdown m-2">
       
-      @if(!Auth::user()->isadmin)
-        <a title="Cart" class="btn btn-primary m-2" href = "{{route('cart')}}">
-          <i class="fa fa-shopping-cart"></i>
-        </a> 
-      @endif
+            <button class="btn btn-secondary dropdown-toggle" id = "categoriesDropdown" data-toggle="dropdown">
+              Categories
+            </button>
+            <nav class="dropdown-menu">
+              <a class="dropdown-item" href="{{url('products/categories/CPU')}}">CPUs</a>
+              <a class="dropdown-item" href="{{url('products/categories/GPU')}}">GPUs</a>
+              <a class="dropdown-item" href="{{url('products/categories/Motherboard')}}">Motherboards</a>
+              <a class="dropdown-item" href="{{url('products/categories/Storage')}}">Storage</a>
+              <a class="dropdown-item" href="{{url('products/categories/PcCase')}}">Cases</a>
+              <a class="dropdown-item" href="{{url('products/categories/Cooler')}}">Coolers</a>
+              <a class="dropdown-item" href="{{url('products/categories/PowerSupply')}}">Power Supplies</a>
+              <a class="dropdown-item" href="{{url('products/categories/Other')}}">Other</a>
+            </nav>
+          </div>
+        </li>
+        <li class = "w-100">
+          <!-- Search bar -->
+          <form class="d-flex" method = "POST" action = "{{route('search')}}">
+            @csrf
+            
+            <input class="form-control me-2" type="search" name = "search" placeholder="Search">
+          </form>
+        </li>
+      </ul>  
+      
+      <!-- User features -->
+      <div class = "navbar-nav d-flex flex-row">
+        @if (Auth::check())
+          <a title="Profile" class="btn btn-primary m-2" href = "{{route('profile')}}">
+            <i class="fa fa-user"></i>
+            <span>{{Auth::user()->username}}</span>
+          </a> 
 
-      <a title="Logout" class="btn btn-primary m-2" href = "{{route('logout')}}">
-        <i class="fa fa-sign-out"></i>
-      </a> 
+          <a title="Notifications" class="btn btn-primary m-2" href = "{{route('home')}}">    {{-- Change this --}}
+              <i class="fa fa-bell"></i>
+            </a> 
+          
+          @if(!Auth::user()->isadmin)
+            <a title="Cart" class="btn btn-primary m-2" href = "{{route('cart')}}">
+              <i class="fa fa-shopping-cart"></i>
+            </a> 
+          @endif
 
-    @else
-      <a title="Login" class="btn btn-primary m-2" href = "{{ route('login') }}"> 
-        <i class="fa fa-sign-in"></i>
-        <span>Login</span>
-      </a>
-    @endif
+          <a title="Logout" class="btn btn-primary m-2" href = "{{route('logout')}}">
+            <i class="fa fa-sign-out"></i>
+          </a> 
+
+        @else
+          <a title="Login" class="btn btn-primary m-2" href = "{{ route('login') }}"> 
+            <i class="fa fa-sign-in"></i>
+            <span>Login</span>
+          </a>
+        @endif
+      </div>
+    </div>
   </div>
+</nav>
 
-</header>
--- resources/views/partials/admin/order_entry.blade.php
+++ resources/views/partials/admin/order_entry.blade.php
@@ -1,71 +1,48 @@
 <div class="card m4-2 mb-4">
   <div class="card-body">
-    <div class="d-flex justify-content-between">
+    <div class="d-flex">
       <h6>
-        <strong>Order number {{$entry['order']->id}}</strong>
+        <strong>Order #{{$entry->id}}</strong>
       </h6> 
-
-      <span>
-        {{$entry['order']->orderdate}} -> {{$entry['order']->orderdate}}
-      </span>
     </div>
 
     <div class="d-flex flex-column mt-2">
       <span>
         {{
-          $entry['address']->streetname.' '
-          .$entry['address']->streetnumber.' '
-          .$entry['address']->aptnumber.' '
-          .$entry['address']->floor
+          $entry->streetname.' '
+          .$entry->streetnumber.' '
+          .$entry->aptnumber.' '
+          .$entry->floor
         }}
       </span>
 
       <span>
-        {{$entry['address']->zipcode}}
+        {{$entry->zipcode}}
       </span>
-    </div>
-    
-    <div class="d-flex flex-column mt-2">
-      <h6>
-        <strong>Items</strong>
-      </h6> 
-
-      <ul>
-        @foreach($entry['products'] as $product)
-          <li>
-            <a class = "text-dark" href={{url('products/'.$product->id)}}>
-              {{$product->name}} - <strong class = "text-primary">{{$product->price}}€</strong>
-            </a>
-          </li>
-        @endforeach
-      </ul>
-      
     </div>
 
     <div class="d-flex justify-content-between mt-1 align-items-center">
-      <h5 class = "price">{{$entry['total']}}€</h5>
-
-      @if($entry['order']->orderstatus == 'Processing')
+      @if($entry->orderstatus == 'Processing')
         <span class = "text-secondary">
           <i class="fa fa-ellipsis-h"></i>
       
-      @elseif($entry['order']->orderstatus == 'Accepted')
+      @elseif($entry->orderstatus == 'Accepted')
         <span class = "text-teal-500">
           <i class="fa fa-check"></i>
 
-      @elseif($entry['order']->orderstatus == 'Packed')
+      @elseif($entry->orderstatus == 'Packed')
         <span class = "text-orange-500">
           <i class="fa fa-gift"></i>  
 
-      @elseif($entry['order']->orderstatus == 'Shipped')
+      @elseif($entry->orderstatus == 'Shipped')
         <span class = "text-info">
           <i class="fa fa-ship"></i>  
       
-      @elseif($entry['order']->orderstatus == 'Cancelled by Store')
+      @elseif($entry->orderstatus == 'Cancelled by Store')
         <span class = "text-red-500">
           <i class="fa fa-ban"></i> 
       
-      @elseif($entry['order']->orderstatus == 'Cancelled by Customer')
+      @elseif($entry->orderstatus == 'Cancelled by Customer')
         <span class = "text-red-500">
           <i class="fa fa-ban"></i>
           
@@ -74,7 +51,7 @@
           <i class="fa fa-truck"></i>    
 
       @endif
-      {{$entry['order']->orderstatus}}
+      {{$entry->orderstatus}}
       </span>
     </div>
   </div>
